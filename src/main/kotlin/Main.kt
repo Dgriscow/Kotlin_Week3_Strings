abe4d5ac454371912d42bac8eb5051e58e193c53
dgriscow <dgrischow@students.mchenry.edu>
2023-08-29 15:49:42 -0500

    finnished strings assignment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    var q1 = arrayOf<String>("What is the name of the programming language used to shape the front end of websites?", "html")
+    var q2 = arrayOf<String>("What programming language is most popular right now?", "python")
+    var q3 = arrayOf<String>("What programming language is best used for handling relational structured data?", "sql")
+    var q4 = arrayOf<String>("What is the most popular low level programming language for game development?", "c++")
+    var q5 = arrayOf<String>("What is the name of googles nosql database?", "Firebase")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var questions = arrayOf(q1, q2, q3, q4, q5)
+
+    var correctCount:Int = 0
+
+    for (question in questions){
+        println(question[0])
+
+        var userEntry = readln()
+
+        if (userEntry.contains(question[1], ignoreCase = true)){
+            println("Correct")
+            correctCount++
+        }else{
+            println("Incorrect")
+        }
+        println("You Got: $correctCount Questions Right!")
+
+
+    }
+
 }
